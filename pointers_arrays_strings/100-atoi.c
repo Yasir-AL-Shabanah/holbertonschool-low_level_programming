3d794073967eae2ec1c0281fedea05fffea6e0b3
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 19:50:35 +0000

    Pointers/Strings: refactor _atoi into helpers to satisfy Betty (no >40-line function)

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,65 +2,90 @@
 #include <limits.h>
 
 /**
- * _atoi - convert a string to an integer
- * @s: input string
- *
- * Return: converted integer (handles INT_MIN correctly)
- */
+* is_space - check if char is a standard whitespace
+* @c: input char
+* Return: 1 if whitespace, 0 otherwise
+*/
+static int is_space(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' ||
+	    c == '\r' || c == '\f' || c == '\v')
+		return (1);
+	return (0);
+}
+
+/**
+* parse_sign - parse consecutive + and - signs and advance index
+* @s: input string
+* @i: pointer to current index (updated)
+* Return: +1 or -1
+*/
+static int parse_sign(const char *s, int *i)
+{
+	int sign = 1;
+
+	while (s[*i] == '+' || s[*i] == '-')
+	{
+		if (s[*i] == '-')
+			sign = -sign;
+		(*i)++;
+	}
+	return (sign);
+}
+
+/**
+* accumulate_digits - accumulate digits with overflow handling
+* @s: input string
+* @i: starting index of first digit
+* @sign: +1 or -1
+* Return: converted integer with sign, INT_MAX/INT_MIN on overflow
+*/
+static int accumulate_digits(const char *s, int i, int sign)
+{
+	int res = 0;
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		int d = s[i] - '0';
+
+		if (sign == 1)
+		{
+			if (res > (INT_MAX - d) / 10)
+				return (INT_MAX);
+			res = res * 10 + d;
+		}
+		else
+		{
+			/* for negative, allow magnitude up to INT_MAX + 1 */
+			if (res > (INT_MAX - d + 1) / 10)
+				return (INT_MIN);
+			res = res * 10 + d;
+		}
+		i++;
+	}
+	return (sign == 1 ? res : -res);
+}
+
+/**
+* _atoi - convert a string to an integer
+* @s: input C-string
+* Return: integer value, or 0 if no digits
+*/
 int _atoi(char *s)
 {
-	unsigned int num = 0;
-	int sign = 1;
-	int started = 0;
-	unsigned int pos_div10 = (unsigned int)(INT_MAX / 10);
-	unsigned int pos_mod10 = (unsigned int)(INT_MAX % 10);
-	unsigned int neg_abs = (unsigned int)INT_MAX + 1U;
-	unsigned int neg_div10 = neg_abs / 10;
-	unsigned int neg_mod10 = neg_abs % 10;
+	int i = 0, sign, has_digit = 0, val;
 
-	while (*s != '\0')
-	{
-		if (*s == '+' || *s == '-')
-		{
-			if (started)
-				break;
-			if (*s == '-')
-				sign = -sign;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			unsigned int d = (unsigned int)(*s - '0');
+	while (s[i] && is_space(s[i]))
+		i++;
 
-			started = 1;
-			if (sign > 0)
-			{
-				if (num > pos_div10 ||
-				    (num == pos_div10 && d > pos_mod10))
-					return (INT_MAX);
-				num = (num * 10U) + d;
-			}
-			else
-			{
-				if (num > neg_div10 ||
-				    (num == neg_div10 && d > neg_mod10))
-					return (INT_MIN);
-				num = (num * 10U) + d;
-			}
-		}
-		else if (started)
-		{
-			break;
-		}
-		s++;
-	}
+	sign = parse_sign(s, &i);
 
-	if (!started)
+	if (s[i] >= '0' && s[i] <= '9')
+		has_digit = 1;
+
+	val = accumulate_digits(s, i, sign);
+
+	if (!has_digit)
 		return (0);
-	if (sign < 0)
-	{
-		if (num == neg_abs)
-			return (INT_MIN);
-		return (-(int)num);
-	}
-	return ((int)num);
+	return (val);
 }
