8b7e849459195174749152ac0a3e400293fc8c8b
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 18:43:12 +0000

    Functions/Task 6: implement _abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - compute the absolute value of an integer
+* @n: the integer to process
+* Return: absolute value of n
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
