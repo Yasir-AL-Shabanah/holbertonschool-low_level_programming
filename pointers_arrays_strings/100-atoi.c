a48efed9de100631289675da514e4bd393b6606a
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 20:34:41 +0000

    Task 10: implement _atoi with sign handling and overflow-safe accumulation

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,27 @@
 #include "main.h"
 #include <limits.h>
 int _atoi(char *s){
-int i=0,sign=1,started=0,res=0,d;
+int i=0,sign=1,res=0,d,start=0;
 while(s[i]){
-if(!started){
+if(!start){
 if(s[i]=='-')sign*=-1;
 else if(s[i]=='+'){}
-else if(s[i]>='0'&&s[i]<='9')started=1;
+else if(s[i]>='0'&&s[i]<='9')start=1;
 }
-if(started){
+if(start){
 if(s[i]>='0'&&s[i]<='9'){
 d=s[i]-'0';
 if(sign==1){
 if(res>INT_MAX/10||(res==INT_MAX/10&&d>INT_MAX%10))return INT_MAX;
 res=res*10+d;
 }else{
-if(res< (INT_MIN+d)/10)return INT_MIN;
+if(res<(INT_MIN+d)/10)return INT_MIN;
 res=res*10-d;
 }
-}else if(res!=0|| (sign==-1&&started))break;
+}else break;
 }
 i++;
 }
-if(!started)return 0;
-if(sign==1)return res>=0?res:INT_MAX;
+if(!start)return 0;
 return res;
 }
