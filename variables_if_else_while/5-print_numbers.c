6aee8c67de7ab1cec38df88e9dd4d0c9a2344d2f
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 15:57:31 +0000

    Variables/Task 5: Print numbers 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Print single digit numbers from 0 to 9
+* Return: 0 on success
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
