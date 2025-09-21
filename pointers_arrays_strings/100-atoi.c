52ad8f9ed07252955ffc74884f1c0aed11e29a8f
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 19:53:36 +0000

    Pointers/Strings: rewrite _atoi (no arrays, single function, overflow clamp, Betty-compliant)

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,90 +2,57 @@
 #include <limits.h>
 
 /**
-* is_space - check if char is a standard whitespace
-* @c: input char
-* Return: 1 if whitespace, 0 otherwise
-*/
-static int is_space(char c)
-{
-	if (c == ' ' || c == '\t' || c == '\n' ||
-	    c == '\r' || c == '\f' || c == '\v')
-		return (1);
-	return (0);
-}
-
-/**
-* parse_sign - parse consecutive + and - signs and advance index
-* @s: input string
-* @i: pointer to current index (updated)
-* Return: +1 or -1
-*/
-static int parse_sign(const char *s, int *i)
-{
-	int sign = 1;
-
-	while (s[*i] == '+' || s[*i] == '-')
-	{
-		if (s[*i] == '-')
-			sign = -sign;
-		(*i)++;
-	}
-	return (sign);
-}
-
-/**
-* accumulate_digits - accumulate digits with overflow handling
-* @s: input string
-* @i: starting index of first digit
-* @sign: +1 or -1
-* Return: converted integer with sign, INT_MAX/INT_MIN on overflow
-*/
-static int accumulate_digits(const char *s, int i, int sign)
-{
-	int res = 0;
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		int d = s[i] - '0';
-
-		if (sign == 1)
-		{
-			if (res > (INT_MAX - d) / 10)
-				return (INT_MAX);
-			res = res * 10 + d;
-		}
-		else
-		{
-			/* for negative, allow magnitude up to INT_MAX + 1 */
-			if (res > (INT_MAX - d + 1) / 10)
-				return (INT_MIN);
-			res = res * 10 + d;
-		}
-		i++;
-	}
-	return (sign == 1 ? res : -res);
-}
-
-/**
 * _atoi - convert a string to an integer
 * @s: input C-string
 * Return: integer value, or 0 if no digits
+*
+* Rules:
+* - Any characters may precede the number.
+* - Every '-' before the first digit flips the sign; '+' is ignored.
+* - Parse the first contiguous digit sequence only.
+* - Clamp on overflow to INT_MAX / INT_MIN.
 */
 int _atoi(char *s)
 {
-	int i = 0, sign, has_digit = 0, val;
+	int sign = 1, started = 0, res = 0;
 
-	while (s[i] && is_space(s[i]))
-		i++;
+	while (*s)
+	{
+		if (!started && (*s == '-' || *s == '+'))
+		{
+			if (*s == '-')
+				sign = -sign;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			int d = *s - '0';
 
-	sign = parse_sign(s, &i);
+			started = 1;
 
-	if (s[i] >= '0' && s[i] <= '9')
-		has_digit = 1;
+			/* overflow-safe accumulation */
+			if (sign == 1)
+			{
+				if (res > (INT_MAX - d) / 10)
+					return (INT_MAX);
+				res = res * 10 + d;
+			}
+			else
+			{
+				/* allow magnitude up to INT_MAX + 1 */
+				if (res > (INT_MAX - d + 1) / 10)
+					return (INT_MIN);
+				res = res * 10 + d;
+			}
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
+	}
 
-	val = accumulate_digits(s, i, sign);
-
-	if (!has_digit)
+	if (!started)
 		return (0);
-	return (val);
+
+	return (sign == 1 ? res : -res);
 }
