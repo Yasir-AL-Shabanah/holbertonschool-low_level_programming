d077797db956ee162d145b65404ebf55468def16
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 20:32:24 +0000

    Task 10: implement _atoi with sign handling and overflow-safe accumulation

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,50 +1,28 @@
 #include "main.h"
 #include <limits.h>
-/**
-* acc_pos - accumulate positive with overflow guard
-* @res: current value (>=0)
-* @d: digit 0..9
-* Return: INT_MAX if overflow else new value
-*/
-static int acc_pos(int res,int d)
-{
-int lim=INT_MAX/10,limd=INT_MAX%10;
-if(res>lim||(res==lim&&d>limd))return(INT_MAX);
-return(res*10+d);
+int _atoi(char *s){
+int i=0,sign=1,started=0,res=0,d;
+while(s[i]){
+if(!started){
+if(s[i]=='-')sign*=-1;
+else if(s[i]=='+'){}
+else if(s[i]>='0'&&s[i]<='9')started=1;
 }
-/**
-* acc_neg - accumulate negative magnitude with overflow guard
-* @res: current magnitude (>=0)
-* @d: digit 0..9
-* Return: INT_MIN if overflow else new magnitude
-*/
-static int acc_neg(int res,int d)
-{
-int lim=INT_MAX/10,limd=INT_MAX%10;
-if(res>lim||(res==lim&&d>limd+1))return(INT_MIN);
-return(res*10+d);
+if(started){
+if(s[i]>='0'&&s[i]<='9'){
+d=s[i]-'0';
+if(sign==1){
+if(res>INT_MAX/10||(res==INT_MAX/10&&d>INT_MAX%10))return INT_MAX;
+res=res*10+d;
+}else{
+if(res< (INT_MIN+d)/10)return INT_MIN;
+res=res*10-d;
 }
-/**
-* _atoi - convert string to int (handles +/- and overflow)
-* @s: input string
-* Return: number, 0 if no digits
-*/
-int _atoi(char *s)
-{
-int sign=1,started=0,res=0;
-if(!s)return(0);
-while(*s)
-{
-if(!started&&(*s=='-'||*s=='+')){if(*s=='-')sign=-sign;}
-else if(*s>='0'&&*s<='9')
-{
-int d=*s-'0',t;started=1;
-if(sign==1){t=acc_pos(res,d);if(t==INT_MAX)return(INT_MAX);res=t;}
-else{t=acc_neg(res,d);if(t==INT_MIN)return(INT_MIN);res=t;}
+}else if(res!=0|| (sign==-1&&started))break;
 }
-else if(started){break;}
-s++;
+i++;
 }
-if(!started)return(0);
-return(sign==1?res:-res);
+if(!started)return 0;
+if(sign==1)return res>=0?res:INT_MAX;
+return res;
 }
