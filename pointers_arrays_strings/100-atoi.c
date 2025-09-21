380c457fe97c0756cb15eeaca9e0741e0e75b29b
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 20:43:35 +0000

    Task 10: pointer-only, 2 functions (<40 lines), multi-sign, overflow-safe _atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,58 +1,62 @@
 #include "main.h"
 #include <limits.h>
 
-/**
- * _atoi - convert string to int (handles multiple signs and overflow)
- * @s: input string
- * Return: converted int, or 0 if no digits
- */
+static char *goto_digits(char *p,int *sign)
+{
+	int allow = 1;
+
+	while (*p)
+	{
+		if (*p == ' ' || *p == '\t' || *p == '\n' ||
+		    *p == '\r' || *p == '\v' || *p == '\f')
+		{
+			p++;
+			continue;
+		}
+		if ((*p == '+' || *p == '-') && allow)
+		{
+			if (*p == '-')
+				*sign *= -1;
+			p++;
+			continue;
+		}
+		if (*p >= '0' && *p <= '9')
+			return (p);
+		allow = 0;
+		p++;
+	}
+	return (0);
+}
+
 int _atoi(char *s)
 {
-	int i = 0;
 	int sign = 1;
-	int started = 0;
 	int res = 0;
+	char *p = goto_digits(s, &sign);
 
-	while (s[i])
-	{
-		if (!started)
-		{
-			if (s[i] == '-')
-				sign *= -1;
-			else if (s[i] == '+')
-				;
-			else if (s[i] >= '0' && s[i] <= '9')
-				started = 1;
-		}
-		if (started)
-		{
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				int d = s[i] - '0';
-
-				if (sign == 1)
-				{
-					if (res > INT_MAX / 10 ||
-					    (res == INT_MAX / 10 && d > INT_MAX % 10))
-						return (INT_MAX);
-					res = res * 10 + d;
-				}
-				else
-				{
-					if (res > 214748364 ||
-					    (res == 214748364 && d > 8))
-						return (INT_MIN);
-					res = res * 10 + d;
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-		i++;
-	}
-	if (!started)
+	if (!p)
 		return (0);
+	while (*p >= '0' && *p <= '9')
+	{
+		int d = *p - '0';
+
+		if (sign == 1)
+		{
+			if (res > INT_MAX / 10 ||
+			    (res == INT_MAX / 10 && d > INT_MAX % 10))
+				return (INT_MAX);
+			res = res * 10 + d;
+		}
+		else
+		{
+			int nd = (INT_MAX % 10) + 1;
+
+			if (res > INT_MAX / 10 ||
+			    (res == INT_MAX / 10 && d > nd))
+				return (INT_MIN);
+			res = res * 10 + d;
+		}
+		p++;
+	}
 	return (sign == 1 ? res : -res);
 }
