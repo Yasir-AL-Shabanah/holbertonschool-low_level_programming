054c257a73cbbb2a37a9c797fb24f7f83db94206
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 15:56:24 +0000

    Variables/Task 4: Print alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print lowercase alphabet except q and e
+* Return: 0 on success
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
