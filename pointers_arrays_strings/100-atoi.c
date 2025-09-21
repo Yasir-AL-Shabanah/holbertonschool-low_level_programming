a8dce1fdf6bc1f86b6d5efbdce0c73bf63cdd90b
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-09-21 19:45:04 +0000

    Pointers/Strings: fix puts2 for long strings; robust _atoi with INT_MIN handling and Betty-compliant

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,67 @@
 #include "main.h"
+#include <limits.h>
 
 /**
 * _atoi - convert a string to an integer
 * @s: input string
-* Return: converted int, or 0 if no numbers found
+* Return: converted integer (handles INT_MIN correctly)
 */
 int _atoi(char *s)
 {
-int i = 0;
+unsigned int num = 0;
 int sign = 1;
 int started = 0;
-int res = 0;
+/* حدود آمنة مشتقة من INT_MAX و |INT_MIN| بدون أرقام صلبة */
+unsigned int pos_div10 = (unsigned int)(INT_MAX / 10);
+unsigned int pos_mod10 = (unsigned int)(INT_MAX % 10);
+unsigned int neg_abs = (unsigned int)INT_MAX + 1U; /* 2147483648 on 32-bit int */
+unsigned int neg_div10 = neg_abs / 10;
+unsigned int neg_mod10 = neg_abs % 10;
 
-while (s[i] != '\0')
+while (*s != '\0')
 {
-if (s[i] == '-' && started == 0)
+if (*s == '+' || *s == '-')
+{
+if (started)
+break;
+if (*s == '-')
 sign = -sign;
-else if (s[i] == '+' && started == 0)
-{
 }
-else if (s[i] >= '0' && s[i] <= '9')
+else if (*s >= '0' && *s <= '9')
 {
+unsigned int d = (unsigned int)(*s - '0');
 started = 1;
-res = (res * 10) + (s[i] - '0');
+if (sign > 0)
+{
+/* تحقق تجاوز الموجب */
+if (num > pos_div10 || (num == pos_div10 && d > pos_mod10))
+return INT_MAX;
+num = (num * 10U) + d;
+}
+else
+{
+/* نسمح بالمقدار حتى 2147483648 لأجل INT_MIN */
+if (num > neg_div10 || (num == neg_div10 && d > neg_mod10))
+return INT_MIN;
+num = (num * 10U) + d;
+}
 }
 else if (started)
+{
 break;
-i++;
 }
-return (res * sign);
+s++;
+}
+
+if (!started)
+return 0;
+
+if (sign < 0)
+{
+/* إن كان المقدار 2147483648 نُعيد INT_MIN، وإلا السالب العادي */
+if (num == neg_abs)
+return INT_MIN;
+return -(int)num;
+}
+return (int)num;
 }
--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
-* puts2 - print every other character of a string starting with the first
-* @str: pointer to string
+* puts2 - print every other character of a string, starting with the first
+* @str: input string
 * Return: void
 */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
+int i;
+
+if (str == 0)
 {
+_putchar('\n');
+return;
+}
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if ((i % 2) == 0)
 _putchar(str[i]);
-i += 2;
 }
 _putchar('\n');
 }
