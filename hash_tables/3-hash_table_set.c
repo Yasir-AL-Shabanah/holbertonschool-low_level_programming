6697ffa51e48177d6cf59566ed9b5a6b20d09127
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-10-26 18:11:28 +0000

    Task 3: set() under 40 lines (helpers + Betty)

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,5 +1,38 @@
 #include "hash_tables.h"
 
+static int update_node(hash_node_t *node, const char *value)
+{
+char *vdup = strdup(value);
+
+if (!vdup)
+return (0);
+free(node->value);
+node->value = vdup;
+return (1);
+}
+
+static hash_node_t *node_new(const char *key, const char *value)
+{
+hash_node_t *n = malloc(sizeof(*n));
+char *kdup, *vdup;
+
+if (!n)
+return (NULL);
+kdup = strdup(key);
+vdup = strdup(value);
+if (!kdup || !vdup)
+{
+free(n);
+free(kdup);
+free(vdup);
+return (NULL);
+}
+n->key = kdup;
+n->value = vdup;
+n->next = NULL;
+return (n);
+}
+
 /**
  * hash_table_set - add or update a key/value
  * @ht: hash table
@@ -10,47 +43,21 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-char *kdup, *vdup;
 unsigned long int idx;
-hash_node_t *node, *head;
+hash_node_t *node;
 
 if (!ht || !key || !*key || !value)
 return (0);
 
 idx = key_index((const unsigned char *)key, ht->size);
-head = ht->array[idx];
-
-for (node = head; node; node = node->next)
-{
+for (node = ht->array[idx]; node; node = node->next)
 if (strcmp(node->key, key) == 0)
-{
-vdup = strdup(value);
-if (!vdup)
-return (0);
-free(node->value);
-node->value = vdup;
-return (1);
-}
-}
+return (update_node(node, value));
 
-node = malloc(sizeof(*node));
+node = node_new(key, value);
 if (!node)
 return (0);
-
-kdup = strdup(key);
-vdup = strdup(value);
-if (!kdup || !vdup)
-{
-free(node);
-free(kdup);
-free(vdup);
-return (0);
-}
-
-node->key = kdup;
-node->value = vdup;
-node->next = head;
+node->next = ht->array[idx];
 ht->array[idx] = node;
-
 return (1);
 }
